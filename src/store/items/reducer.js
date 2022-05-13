a08b34a304196cdc04b6212e3b0cc37330109765
Nilotpal Deka <iamnilotpaldeka@gmail.com>
2022-05-13 23:15:05 +0530

    Add immer for mutating state directly

--- src/store/items/reducer.js
+++ src/store/items/reducer.js
@@ -1,3 +1,4 @@
+import produce from 'immer';
 import {
   ITEM_ADDED,
   ITEM_PRICE_UPDATED,
@@ -12,28 +13,22 @@ export const initialItems = [
   { uuid: id++, name: 'Vegan Ham', price: 12, quantity: 1 }
 ];
 
-export const reducer = (state = initialItems, action) => {
+export const reducer = produce((state = initialItems, action) => {
   if (action.type === ITEM_ADDED) {
     const item = { uuid: id++, quantity: 1, ...action.payload };
-    return [...state, item];
+    state.push(item);
   }
 
   if (action.type === ITEM_REMOVED)
     return state.filter((item) => item.uuid !== action.uuid);
 
-  if (action.type === ITEM_PRICE_UPDATED)
-    return state.map((item) => {
-      if (item.uuid === action.payload.uuid)
-        return { ...item, price: action.payload.price };
-      return item;
-    });
+  if (action.type === ITEM_PRICE_UPDATED) {
+    const item = state.find((item) => item.uuid === action.payload.uuid);
+    item.price = action.payload.price;
+  }
 
-  if (action.type === ITEM_QUANTITY_UPDATED)
-    return state.map((item) => {
-      if (item.uuid === action.payload.uuid)
-        return { ...item, quantity: action.payload.quantity };
-      return item;
-    });
-
-  return state;
-};
+  if (action.type === ITEM_QUANTITY_UPDATED) {
+    const item = state.find((item) => item.uuid === action.payload.uuid);
+    item.quantity = action.payload.quantity;
+  }
+}, initialItems);
